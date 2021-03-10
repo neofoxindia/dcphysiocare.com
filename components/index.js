2c3e0c4b688b33ce98440fe76ff2cf1847f80b25
its-ash <its-ash@users.noreply.github.com>
2021-03-10 23:43:34 +0000

    deploy: 39cb67cca3bc45b3038a6cf937cdfef72a062b42

--- components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as BaseHeader } from '../../components/BaseHeader.vue'
-
-export const LazyBaseHeader = import('../../components/BaseHeader.vue' /* webpackChunkName: "components/base-header" */).then(c => c.default || c)
