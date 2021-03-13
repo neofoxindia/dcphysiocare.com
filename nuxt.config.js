5af718a8037cf9cff23d501ad1cb0e58532ecb66
Ashvini Jangid <ashvinijangid@gmail.com>
2021-03-14 00:34:07 +0530

    FIx

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,12 +22,8 @@ export default {
         type: 'text/css',
         href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Raleway:wght@300;400&family=Poppins:wght@300;500;700;800&display=swap'
       },
-
-
       // ---------- Fonts ----------
       {rel: 'stylesheet', type: 'text/css', href: '/css/icons/fontawesome/css/style.css'},
-      {rel: 'stylesheet', type: 'text/css', href: '/css/icons/icon2/style.css'},
-      {rel: 'stylesheet', type: 'text/css', href: '/css/ilmosys-icon.css'},
       {rel: 'stylesheet', type: 'text/css', href: '/css/style.css'},
     ],
     script: [
